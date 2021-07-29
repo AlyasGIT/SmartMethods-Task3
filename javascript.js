e557e0bff5255b847a9858dd283611b9c065289e
AlyasGIT <86851782+AlyasGIT@users.noreply.github.com>
2021-07-29 18:49:36 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,47 @@
+var motor1 = document.getElementById("motor1");
+var motor1Out = document.getElementById("motor1Out");
+motor1Out.innerHTML = "Motor 1: " + motor1.value;
+
+motor1.oninput = function() {
+    motor1Out.innerHTML = "Motor 1: " + this.value;
+} 
+
+var motor2 = document.getElementById("motor2");
+var motor2Out = document.getElementById("motor2Out");
+motor2Out.innerHTML = "Motor 2: " + motor2.value;
+
+motor2.oninput = function() {
+    motor2Out.innerHTML = "Motor 2: " + motor2.value;
+} 
+
+var motor3 = document.getElementById("motor3");
+var motor3Out = document.getElementById("motor3Out");
+motor3Out.innerHTML = "Motor 3: " + motor3.value;
+
+motor3.oninput = function() {
+    motor3Out.innerHTML = "Motor 3: " + this.value;
+} 
+
+var motor4 = document.getElementById("motor4");
+var motor4Out = document.getElementById("motor4Out");
+motor4Out.innerHTML = "Motor 4: " + motor4.value;
+
+motor4.oninput = function() {
+    motor4Out.innerHTML = "Motor 4: " + this.value;
+} 
+
+var motor5 = document.getElementById("motor5");
+var motor5Out = document.getElementById("motor5Out");
+motor5Out.innerHTML = "Motor 5: " + motor5.value;
+
+motor5.oninput = function() {
+    motor5Out.innerHTML = "Motor 5: " + this.value;
+} 
+
+var motor6 = document.getElementById("motor6");
+var motor6Out = document.getElementById("motor6Out");
+motor6Out.innerHTML = "Motor 6: " + motor6.value;
+
+motor6.oninput = function() {
+    motor6Out.innerHTML = "Motor 6: " + this.value;
+} 
